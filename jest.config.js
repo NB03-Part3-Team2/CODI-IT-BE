f63ef70b723b37c3cbd81898b91ee8ee8de9dbbe
Seracine <csyo4025@gmail.com>
2025-10-23 18:12:02 +0900

    feat(test): jest용 샘플 코드 및 설정 업데이트

--- jest.config.js
+++ jest.config.js
@@ -170,9 +170,7 @@ const config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: ['\\node_modules\\', '<rootDir>/src/modules/sample'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
--- src/modules/sample/sampleRepo.ts
+++ src/modules/sample/sampleRepo.ts
@@ -1,9 +1,8 @@
 import { sampleDTO } from './dto/sampleDTO';
-
 class sampleRepository {
-	create = async (sampleDTO: sampleDTO) => {
-		// sampleDTO의 타입은 DTO를 정의하여 수정합니다.
-		/*
+  create = async (sampleDTO: sampleDTO): Promise<sampleDTO> => {
+    // sampleDTO의 타입은 DTO를 정의하여 수정합니다.
+    /*
 		데이터베이스와 상호작용하는 코드를 작성합니다.
 			return await prisma.sample.create({
 				data: {
@@ -11,8 +10,10 @@ class sampleRepository {
 				}
 		});
 		*/
-		console.log(sampleDTO);
-	};
+    console.log(sampleDTO);
+    //실제 구현에서는 데이터베이스 생성 결과를 반환해야 합니다.
+    return sampleDTO;
+  };
 }
 
 export default new sampleRepository();
