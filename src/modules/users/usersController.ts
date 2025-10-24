11fe257057d8f4593df98b5db15f0d0b912af6b8
choYoungWook <wwwe2410@naver.com>
2025-10-24 17:33:30 +0900

    add:주석 추가

--- src/modules/users/usersController.ts
+++ src/modules/users/usersController.ts
@@ -3,6 +3,23 @@ import usersService from '@modules/users/usersService';
 import { CreateUserDto } from '@modules/users/dto/userDTO';
 
 class UsersController {
+  /**
+   * 새로운 유저를 생성합니다.
+   *
+   * 아이디, 이름, 비밀번호 정보를 받아 새로운 유저를 생성합니다.
+   * 데이터가 잘못되었거나 이메일 또는 이름이 중복될 경우 에러를 발생시킵니다.
+   * 서버 오류로 인해 유저 생성에 실패할 수 있습니다.
+   *
+   * @param {Object} req - 요청 객체
+   * @param {Object} res - 응답 객체
+   *
+   * @returns {Object} 생성된 유저 정보 (HTTP 201)
+   *
+   * @throws {ApiError} 400 - 잘못된 요청 데이터
+   * @throws {ApiError} 409 - 이메일 또는 이름 중복
+   * @throws {ApiError} 500 - 서버 내부 오류
+   */
+
   createUser = async (req: Request, res: Response) => {
     const CreateUserDto: CreateUserDto = {
       email: req.body.email,
