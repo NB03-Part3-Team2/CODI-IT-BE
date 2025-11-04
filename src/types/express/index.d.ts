17b2adf2c09b2af5e0efc882d6ed635f21ed32e6
choYoungWook <wwwe2410@naver.com>
2025-11-04 17:32:56 +0900

    chore(review):주석수정

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace Express {
     interface Request {
       user: Pick<User, 'id'>;
-      validatedBody?: any; // ✅ 초기에는 이렇게
+      validatedBody?: any;
       validatedQuery?: any;
       validatedParams?: any;
     }
