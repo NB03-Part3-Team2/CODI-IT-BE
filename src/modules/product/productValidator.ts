62e9a1c2b033e6e53ecbab8a80a0fd03731f8979
Seracine <csyo4025@gmail.com>
2025-11-05 17:24:21 +0900

    refactor(product): validator parseInt 사용 파트 제거

--- src/modules/product/productValidator.ts
+++ src/modules/product/productValidator.ts
@@ -9,9 +9,9 @@ class ProductValidator {
       const parsedBody = {
         name: req.body.name,
         categoryName: req.body.categoryName.toUpperCase(),
-        price: parseInt(req.body.price, 10),
+        price: req.body.price,
         image: req.body.image ?? null,
-        discountRate: req.body.discountRate ? parseInt(req.body.discountRate, 10) : 0,
+        discountRate: req.body.discountRate ?? 0,
         discountStartTime: req.body.discountStartTime,
         discountEndTime: req.body.discountEndTime,
         content: req.body.content,
@@ -31,12 +31,12 @@ class ProductValidator {
     try {
       // 1. 검사할 속성 정의
       const parsedQuery = {
-        page: req.query.page ? parseInt(req.query.page as string, 10) : undefined,
-        pageSize: req.query.pageSize ? parseInt(req.query.pageSize as string, 10) : undefined,
+        page: req.query.page,
+        pageSize: req.query.pageSize,
         search: req.query.search as string | undefined,
         sort: req.query.sort as string | undefined,
-        priceMin: req.query.priceMin ? parseInt(req.query.priceMin as string, 10) : undefined,
-        priceMax: req.query.priceMax ? parseInt(req.query.priceMax as string, 10) : undefined,
+        priceMin: req.query.priceMin,
+        priceMax: req.query.priceMax,
         size: req.query.size as string | undefined,
         favoriteStore: req.query.favoriteStore ? (req.query.favoriteStore as string) : undefined,
         categoryName: req.query.categoryName
