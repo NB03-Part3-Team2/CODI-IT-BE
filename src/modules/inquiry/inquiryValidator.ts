1cf0489cdb6639d3f78ae6d724e822b9b084ba58
Seracine <csyo4025@gmail.com>
2025-11-10 17:14:13 +0900

    chore(inquiry): validator 주석 추가

--- src/modules/inquiry/inquiryValidator.ts
+++ src/modules/inquiry/inquiryValidator.ts
@@ -6,6 +6,7 @@ import { productIdSchema } from '@modules/product/dto/productDTO';
 class InquiryValidator {
   validateCreateInquiry: RequestHandler = async (req, res, next) => {
     try {
+      // 1. 검사할 속성 정의
       const parsedBody = {
         title: req.body.title,
         content: req.body.content,
@@ -16,6 +17,7 @@ class InquiryValidator {
         id: req.params.productId,
       };
 
+      // 2. 스키마에 맞춰 유효성 검사
       req.validatedBody = await createInquirySchema.parseAsync(parsedBody);
       req.validatedParams = await productIdSchema.parseAsync(parsedParams);
 
@@ -27,10 +29,12 @@ class InquiryValidator {
 
   validateGetInquiryList: RequestHandler = async (req, res, next) => {
     try {
+      // 1. 검사할 속성 정의
       const parsedParams = {
         id: req.params.productId,
       };
 
+      // 2. 스키마에 맞춰 유효성 검사
       req.validatedParams = await productIdSchema.parseAsync(parsedParams);
 
       next();
