37aba7bf1443c5d47d8a33b42859a29918ed3bda
choYoungWook <wwwe2410@naver.com>
2025-11-11 10:02:59 +0900

    feat(review):route에 유저인증 및 밸리데이션 빠뜨려서 추가

--- src/modules/review/reviewRoute.ts
+++ src/modules/review/reviewRoute.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import reviewController from '@modules/review/reviewController';
+import reviewValidator from '@modules/review/reviewValidator';
+import { authMiddleware } from '@middlewares/authMiddleware';
 
 const reviewRouter = express.Router();
 
-reviewRouter.patch('/:reviewId/', reviewController.updateReview);
+reviewRouter.patch(
+  '/:reviewId',
+  authMiddleware,
+  reviewValidator.validateUpdateReview,
+  reviewController.updateReview,
+);
 
 export default reviewRouter;
