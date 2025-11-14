7b2b96f3c99dbfbc9fba9ab2345da05e2d110c03
choYoungWook <wwwe2410@naver.com>
2025-11-14 15:48:03 +0900

    refactor(notification): 사용하지않는 코드 삭제

--- src/modules/notification/dto/notificationDTO.ts
+++ src/modules/notification/dto/notificationDTO.ts
@@ -1,10 +1,3 @@
-// 알림 타입
-export type NotificationType =
-  | 'OUT_OF_STOCK' // 품절
-  | 'CART_ITEM_OUT_OF_STOCK' // 장바구니 상품 품절
-  | 'NEW_INQUIRY' // 새 문의
-  | 'INQUIRY_ANSWERED'; // 문의 답변
-
 export interface CreateNotificationDto {
   userId: string;
   content: string;
