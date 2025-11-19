ebc6b6737c0aa60b1753920211a34db2e6057c36
choYoungWook <wwwe2410@naver.com>
2025-11-19 09:26:49 +0900

    feat(notification): 상품 업데이트시 전송 복구

--- src/modules/product/productService.ts
+++ src/modules/product/productService.ts
@@ -139,22 +139,26 @@ class ProductService {
     const updatedProduct = await productRepository.updateProduct(productId, repoDto);
 
     // 재고가 품절로 변경된 경우, 장바구니에 담긴 유저들에게 알림 전송
-    const soldOutStocks = updatedProduct.stocks.filter((stock) => stock.quantity === 0);
-    if (soldOutStocks.length > 0) {
-      for (const stock of soldOutStocks) {
-        const cartUserIds = await cartRepository.getUserIdsBySoldOutProduct(
-          productId,
-          stock.size.id,
-        );
+    try {
+      const soldOutStocks = updatedProduct.stocks.filter((stock) => stock.quantity === 0);
+      if (soldOutStocks.length > 0) {
+        for (const stock of soldOutStocks) {
+          const cartUserIds = await cartRepository.getUserIdsBySoldOutProduct(
+            productId,
+            stock.size.id,
+          );
 
-        await notificationService.notifyOutOfStock({
-          sellerId: userId,
-          storeName: store.name,
-          productName: updatedProduct.name,
-          sizeName: stock.size.en,
-          cartUserIds,
-        });
+          await notificationService.notifyOutOfStock({
+            sellerId: userId,
+            storeName: store.name,
+            productName: updatedProduct.name,
+            sizeName: stock.size.en,
+            cartUserIds,
+          });
+        }
       }
+    } catch (error) {
+      console.error('상품 업데이트 중 알림 전송 에러:', error);
     }
 
     // 새 이미지가 제공되고 기존 이미지가 있는 경우, 기존 S3 이미지 삭제
